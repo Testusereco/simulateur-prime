fcda7ab0544ff5ca5826bd983c2e4970ed54a269
Testusereco <testuser1.eco@gmail.com>
2026-09-09 17:05:09 +0200

    Configuration des routes (acces -> simulation) - nettoyage app.html - 22/22 tests verts

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AccesForm } from './components/acces-form/acces-form';
+import { SimulationForm } from './components/simulation-form/simulation-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AccesForm },
+  { path: 'simulation', component: SimulationForm },
+];
